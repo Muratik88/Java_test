acb6c7a9b74f5e494edf51d938c9493ca48c2b68
Murat <muha188@mail.ru>
2019-08-28 13:41:06 +0600

    Comment with notepad++

--- src/DbConnect.java
+++ src/DbConnect.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+//import java.sql.SQLException;
 
 public class DbConnect {
     private Connection getConnection() throws SQLException {
