776c055955020c3c737583adcd31a595f87c9c65
Murat <muha188@mail.ru>
2019-08-28 15:46:47 +0600

    user2 changed

---
+++ src/settings.java
@@ -0,0 +1,14 @@
+public class Evidence {
+    private String serialNo, cTime;
+    private String mValue;
+    private String devType;
+
+
+    public Evidence(String serialNo, String cTime, String mValue, String devType) {
+        this.serialNo = serialNo;
+        this.cTime = cTime;
+        this.mValue = mValue;
+        this.devType = devType;
+    }
+
+}
